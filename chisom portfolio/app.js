6325cd772f2836a262962cdd63e2a2433aefd2b6
Ifeanyi Tochi <82006448+ifytochez@users.noreply.github.com>
2022-02-11 12:02:59 +0100

    Add files via upload

---
+++ chisom portfolio/app.js
@@ -0,0 +1,22 @@
+// const sections = document.querySelectorAll("section");
+// const navContainer = document.querySelector("nav .nav-menu li a");
+
+
+
+// window.addEventListener('scroll', ()=>{
+//   let current = "";
+//   sections.forEach(section =>{
+//     const sectionTop = section.offsetTop;
+//     const sectionHeight = section.clientHeight;
+//     if(pageYoffset >= (sectionTop - sectionHeight/3 )){
+//       current = section.getAttribute("id");
+//     }
+//   })
+//   navContainer.forEach(li =>{
+//     li.classList.remove('active');
+//     if(li.classList.contains(current)){
+//       li.classList.add('active')
+//     }
+//   })
+// })
+
