ec7e94fea7bbb5e60a32ff707e08a7f28cb0a276
ifytochez <ifytochez1@gmail.com>
2022-03-05 18:37:08 +0100

    resolved

--- chisom portfolio/app.js
+++ chisom portfolio/app.js
@@ -1,3 +1,40 @@
+const sections = document.querySelectorAll('section');
+const navLi = document.querySelectorAll('.navcontainer .navbar li');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if(pageYOffset >= (sectionTop - sectionHeight / 3)){
+            current = section.getAttribute('id');
+        }
+    })
+    navLi.forEach( li => {
+        li.classList.remove('activee');
+        if(li.classList.contains(current)){
+            li.classList.add('activee')
+        }
+    })
+})
+
+
+
+// const activePage = window.location.pathname;
+// const navLinks = document.querySelectorAll('nav a'). forEach(
+//     link => {
+//         if(link.href.includes(`${activePage}`)){
+//            link.classList.add('active');
+//         }
+       
+//     }
+// );
+
+
+
+
 // const sections = document.querySelectorAll("section");
 // const navContainer = document.querySelector("nav .nav-menu li a");
 
@@ -19,4 +56,3 @@
 //     }
 //   })
 // })
-
