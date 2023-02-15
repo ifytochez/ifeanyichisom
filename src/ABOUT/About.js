c67cd3f96c3d13a9c709f6861dc7c5559f847e2d
ifeanyi-okoronkwo,Tochi <tochi.okoronkwo@interswitchng.com>
2023-02-15 11:49:12 +0100

    commit

---
+++ src/ABOUT/About.js
@@ -0,0 +1,60 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <div className="about" id="about">
+      <div className="about-container">
+        <div className="about-image">
+          <img
+            className="About-picture"
+            src="/images/chisompic.jpg"
+            alt="abtpix"
+          />
+        </div>
+
+        <div className="aboutwriteup">
+          <h1>
+            Hello! I am <span>Chisom</span>{" "}
+          </h1>
+
+          <p>
+            {" "}
+            I am a professional product designer passionate about creating
+            simple and accessible digital experiences that translate business
+            objectives into solutions.
+          </p>
+
+          <p>
+            I push products to the next level through a design process that
+            includes data-backed user research, rapid iteration, and visually
+            stunning user-focused UI.
+          </p>
+
+          <p>
+            I've worked on digital solutions/products ranging from company’s
+            internal software, B2B products, to eCommerce websites, Fintech, and
+            e-learning platforms.
+          </p>
+
+          <p>
+            I’m always ready to work on interesting and meaningful projects with
+            people and businesses all over the world.
+          </p>
+
+          <p>
+            When I'm not designing, you would find me binge-watching comedy
+            movies, reading a book or sleeping.
+          </p>
+
+          <p>
+            If you'd like to know more or have a project you'd like to discuss,
+            don't hesitate to reach out: ifeanyichisomjane@gmail.com.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import React from "react";
-import './App.css';
-import {BrowserRouter as Router, Routes, Route,} from "react-router-dom";
-import NavBar from "./NavBar";
-import Projects from "./Projects"
-import BorderlessCaseStudy from './BorderlessCaseStudy'
-import CashietCaseStudy from "./CashietCaseStudy";
-import About from "./About";
-import Footer from "./Footer";
-import AiBanc from "./AiBanc";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./NAVBAR/NavBar";
+import Projects from "./PROJECT/Projects";
+import About from "./ABOUT/About";
+import CashietCaseStudy from "./CASHIET/CashietCaseStudy";
+import BorderlessCaseStudy from "./BORDERLESS/BorderlessCaseStudy";
+import AiBanc from "./AIBANC/AiBanc";
+import Footer from "./FOOTER/Footer";
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <NavBar/>
+      <div className="App">
+        <NavBar />
 
-      <Routes>
-      <Route path='/' element={<Projects/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/borderlesscasestudy' element={<BorderlessCaseStudy/>}/>
-      <Route path='/cashietcasestudy' element={<CashietCaseStudy/>}/>
-      <Route path='/aiBanc' element={<AiBanc/>}/>
+        <Routes>
+          <Route path="/" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route
+            path="/borderlesscasestudy"
+            element={<BorderlessCaseStudy />}
+          />
+          <Route path="/cashietcasestudy" element={<CashietCaseStudy />} />
+          <Route path="/aibanc" element={<AiBanc />}></Route>
+        </Routes>
 
-      </Routes>
-
-
-      <Footer/>
-    </div>
-
+        <Footer />
+      </div>
     </Router>
   );
 }
